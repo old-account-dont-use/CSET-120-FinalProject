f1837c524d8211bff1c9973cd2273104d16cf9a4
le-me <zshort4009@stevenscollege.edu>
2023-11-17 15:20:36 -0500

    Login page text inputs

--- src/scripts/account_page.js
+++ src/scripts/account_page.js
@@ -14,13 +14,78 @@ AccountPage.getEntryText = (mode) =>
         default:
         case AccountPage.ENTRY_MODE_NEW:
             return "Sign up"
-        
+
         case AccountPage.ENTRY_MODE_EXISTING:
             return "Login"
 
         case AccountPage.ENTRY_MODE_FORGOT:
             return "Forgot Password"
     }
+}
+
+/*
+*   Sets up the form corresponding to the entry mode
+*/
+AccountPage.setupEntryForm = (form, mode) =>
+{
+	const email = document.createElement("h4")
+	email.innerHTML = "Email Address"
+
+	const email_input = document.createElement("input")
+	email_input.id = "account_entry_email"
+	{
+		email_input.setAttribute("type", "text")
+		email_input.setAttribute("placeholder", "Enter your email address")
+		email_input.setAttribute("required", true)
+	}
+
+	form.appendChild(email)
+	form.appendChild(email_input)
+
+	switch(mode)
+	{
+		default:
+		case AccountPage.ENTRY_MODE_NEW:
+        case AccountPage.ENTRY_MODE_EXISTING:
+        {
+			const password = document.createElement("h4")
+			password.innerHTML = "Password"
+
+			const password_input = document.createElement("input")
+			password_input.id = "account_entry_password"
+			{
+				password_input.setAttribute("type", "password")
+				password_input.setAttribute("minlength", 8)
+				password_input.setAttribute("maxlength", 255)
+				password_input.setAttribute("placeholder", "Enter your desired password")
+				password_input.setAttribute("required", true)
+			}
+
+			form.appendChild(password)
+			form.appendChild(password_input)
+
+			if (mode == AccountPage.ENTRY_MODE_NEW)
+			{
+				const password_input_confirm = document.createElement("input")
+				password_input_confirm.id = "account_entry_password_confirm"
+				{
+					password_input_confirm.setAttribute("type", "password")
+					password_input_confirm.setAttribute("minlength", 8)
+					password_input_confirm.setAttribute("maxlength", 255)
+					password_input_confirm.setAttribute("placeholder", "Confirm your password")
+					password_input_confirm.setAttribute("required", true)
+				}
+				form.appendChild(password_input_confirm)
+			}
+
+			break
+		}
+
+        case AccountPage.ENTRY_MODE_FORGOT:
+        {
+			break
+		}
+	}
 }
 
 /*
@@ -41,9 +106,7 @@ AccountPage.createEntryPanel = (mode) =>
             const form = document.createElement("form")
             form.id = "account_entry_form"
             form.setAttribute("onsubmit", "") // TODO: On form submit action
-            {
-
-            }
+            AccountPage.setupEntryForm(form, mode)
 
             panel.appendChild(title)
             panel.appendChild(form)
--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -16,7 +16,7 @@ Helper.hash = (string) =>
 	if (!Helper.isString(string))
 		string = String(string)
 
-    for(var i = 0, h = 0xDEADBEEF; i < string.length; i++)
+    for (var i = 0, h = 0xDEADBEEF; i < string.length; i++)
         h = Math.imul(h ^ string.charCodeAt(i), 2654435761)
 
     return (h ^ h >>> 16) >>> 0
